5ec97cf256d1c66e55a9c5a10009583e64d237eb
Shae <a.thomas.vc@gmail.com>
2024-04-28 23:15:45 -0700

    Implemented string literals

--- src/caustic/grammar/test.py
+++ src/caustic/grammar/test.py
@@ -1,11 +1,13 @@
 #!/bin/python
 
 #> Imports
+import sys
 import parglare
 import operator
 from pprint import pprint
 
 from caustic import cst
+from caustic.parser import error
 #</Imports
 
 #> Header
@@ -14,17 +16,18 @@ from caustic import cst
 #> Main >/
 test = 'from a.b import c as d, e as f, g;'
 test = '0$3:012012$;'
+#test = 'f"test{{{test}}} t";'
 
 def f(ctx, args, **kwargs):
     global p
     p = (ctx, args, kwargs)
 
 def node_builder(ctx, args, **kwargs):
-    print(ctx, args, kwargs)
     return getattr(getattr(cst, ctx.production.user_meta['mod']),
                    ctx.production.user_meta.get('cls', ctx.production.symbol.name))(**kwargs)
-def node_unpack_builder(ctx, args):
-    return node_builder(ctx, (), **args[0])
+def node_unpack_builder(ctx, args, **kwargs):
+    print(args[ctx.production.user_meta.get('index', 0)])
+    return node_builder(ctx, (), **(args[ctx.production.user_meta.get('index', 0)] | kwargs))
 
 grammar = parglare.Grammar.from_file('./canonical/canonical.pg')
 parser = parglare.GLRParser(grammar, lexical_disambiguation=True, actions={
@@ -36,8 +39,11 @@ parser = parglare.GLRParser(grammar, lexical_disambiguation=True, actions={
     'node': node_builder,
     'unpack_node': node_unpack_builder,
 }, tables=parglare.LALR)
-forest = parser.parse(test)
-tree = forest.get_first_tree()
-parsed = parser.call_actions(tree)
-
-pprint(parsed)
+try:
+    forest = parser.parse(test)
+    tree = forest.get_first_tree()
+    parsed = parser.call_actions(tree)
+except parglare.ParseError as pe:
+    print(error.format_exc(pe), file=sys.stderr)
+else:
+    pprint(parsed)
