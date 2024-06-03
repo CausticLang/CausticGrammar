70358d9aad1802427e16ae6b84fddec2e257561e
Shae <shae.m.vc@proton.me>
2024-06-03 10:44:34 -0700

    Brought grammar up-to-parity with new CausticAST implementation

--- helper.h
+++ helper.h
@@ -8,10 +8,23 @@
     cap_error(auxil, __pcc_ctx, rname, msg, _cap_PCC0s, _cap_PCC0e)
 
 #define _cap_MKNODE(type) \
-    (struct cst_n##type*)malloc(sizeof(struct cst_n##type))
+    (cst_n##type*)malloc(sizeof(cst_n##type))
 #define _cap_MKINITNODE(type, ...) \
-    cst_ninit_##type(_cap_MKNODE(type), _cap_PCC0s, _cap_PCC0e, cap_lno(auxil), cap_cno(auxil, _cap_PCC0s), __VA_ARGS__)
+    cst_binit_##type(malloc(sizeof(cst_Node)), cst_ninit_##type(_cap_MKNODE(type), __VA_ARGS__), _cap_PCC0s, _cap_PCC0e, cap_lno(auxil), cap_cno(auxil, _cap_PCC0s))
+
+#define _cap_ADDPARAM(ptype, type_, name_, val_) \
+    do { \
+        assert(auxil->stack->mark == PROC_PARAMS); \
+        cst_ProcParam* param = malloc(sizeof(cst_ProcParam)); \
+        *param = (cst_ProcParam)CST__PROCEDURES__PROC_PARAM__INIT; \
+        param->type = type_; \
+        param->name = name_; \
+        param->val = val_; \
+        param->param_type = CST__PROCEDURES__PROC_PARAM_TYPE__##ptype; \
+        auxil->stack->data = realloc(auxil->stack->data, ++auxil->stack->auxcount * sizeof(cst_ProcParam*)); \
+        ((cst_ProcParam**)auxil->stack->data)[auxil->stack->auxcount-1] = param; \
+    } while(0)
 
 #define _cap_ADDNODE(node) \
-    cst_node_add(auxil->root, cst_NODEDOWNCAST(node))
-#define _cap_LASTNODE auxil->root->node_count-1
+    cst_ADDNODE(auxil->root, node)
+#define _cap_LASTNODE auxil->root->n_nodes-1
